ad6999ec4cbc820af3f78ab84968e3ee7f63c908
Antonio Calanducci <acaland@Antonios-MacBook-Air.local>
2013-11-12 11:42:25 +0100

    first commit

---
+++ Resources/app.js
@@ -0,0 +1,50 @@
+var win = Ti.UI.createWindow({
+	backgroundColor : "white"
+});
+
+
+
+
+
+var btn = Ti.UI.createButton({
+	title : "Apri View",
+	//bottom : 40,
+	height : 60,
+	width : 180,
+	bottom:40
+});
+
+
+win.add(btn);
+
+var view = Ti.UI.createView({
+	backgroundColor : "blue",
+	width : "90%",
+	height : "90%",
+	borderRadius : 5,
+	visible : false,
+	borderColor: "black",
+	borderWidth: 1
+});
+
+var btnClose = Ti.UI.createButton({
+	title : "Chiudi View",
+	height : 60,
+	width : 180
+});
+
+view.add(btnClose);
+win.add(view);
+
+
+
+btn.addEventListener('click', function () {
+	view.show();
+	Ti.API.info("hai aperto view");
+});
+
+btnClose.addEventListener('click', function() {
+	view.hide();
+});
+
+win.open();
